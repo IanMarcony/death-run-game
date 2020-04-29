5a8b3f344f4d111c98856afa2ac318a34c49d68b
IanMarcony <imarconyls@gmail.com>
2020-04-29 18:27:20 -0400

    Added Assets

---
+++ Assets/codes/arma.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum StateGame { GAMEPLAY,PAUSE}
+
+
+public class arma : MonoBehaviour
+{
+
+    private Rigidbody2D rgArma;
+    public GameObject perdeu;
+    public StateGame estado;
+    public ParticleSystem[] sangue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgArma = GetComponent<Rigidbody2D>();
+        perdeu.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            rgArma.gravityScale = 50;
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag=="Player")
+        {
+            perdeu.SetActive(true);
+            Destroy(col.gameObject);
+            Time.timeScale=0;
+            
+            foreach (ParticleSystem o in sangue )
+            {
+                o.Play();
+            }
+            StartCoroutine("pausa");
+            estado = StateGame.PAUSE;
+        }
+    }
+
+
+    IEnumerator pausa()
+    {
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
+    }
+
+}
---
+++ Assets/codes/player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    private Rigidbody2D rgPlayer;
+    public LayerMask whatsGround;
+    public Transform groundCheck;
+    public bool grounded,jump;
+    public BoxCollider2D stand, under;
+    public float speed;
+    private float h, v;
+    public float jumpForce;
+    private arma controleArma;
+    private int idAnimation;
+    private Animator animPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgPlayer = GetComponent<Rigidbody2D>();
+        controleArma = FindObjectOfType(typeof(arma)) as arma;
+        animPlayer = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        if (controleArma.estado!=StateGame.GAMEPLAY) return;
+        rgPlayer.velocity =new Vector2(h*speed,rgPlayer.velocity.y);
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, 0.002f, whatsGround);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controleArma.estado != StateGame.GAMEPLAY) return;
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+
+        if (v < 0)
+        {
+            idAnimation = 1;
+            under.enabled=true;
+            stand.enabled=false;
+        }
+        else
+        {
+            idAnimation = 0;
+
+            under.enabled = false;
+            stand.enabled = true;
+        }
+
+        if (grounded&&Input.GetButtonDown("Jump"))
+        {
+            
+            rgPlayer.AddForce(new Vector3(rgPlayer.velocity.x, jumpForce,0));
+            
+        }
+        if (!grounded) jump = true;
+        else jump = false;
+        if (jump&&v<0)
+        {
+            rgPlayer.AddForce(new Vector3(rgPlayer.velocity.x, -1*jumpForce, 0));
+        }
+
+
+
+        animPlayer.SetInteger("idAnimation",idAnimation);
+        animPlayer.SetBool("grounded",grounded);
+    }
+}
